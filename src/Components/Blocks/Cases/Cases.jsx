57610b9be92b2c916bad4b163fcd0cb5d60a060e
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2026-01-15 13:57:26 +0300

    Upd main page all positions

--- src/Components/Blocks/Cases/Cases.jsx
+++ src/Components/Blocks/Cases/Cases.jsx
@@ -2,18 +2,18 @@ import React, { useState, useRef } from "react";
 import classes from './Cases.module.css';
 import { Link } from "react-router-dom";
 
-function CaseCard({ imgSrc, title, description, tags }) {
+function CaseCard({ imgSrc, title, description, tags, type }) {
     const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
     const [isHovered, setIsHovered] = useState(false);
     const caseRef = useRef(null);
 
     const handleMouseMove = (e) => {
         if (!caseRef.current) return;
-        
+
         const rect = caseRef.current.getBoundingClientRect();
         const x = e.clientX - rect.left;
         const y = e.clientY - rect.top;
-        
+
         setMousePosition({ x, y });
     };
 
@@ -26,40 +26,153 @@ function CaseCard({ imgSrc, title, description, tags }) {
     };
 
     return (
-        <div 
-            className={`${classes.case} ${classes.type_case}`}
-            ref={caseRef}
-            onMouseMove={handleMouseMove}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-            data-cursor="case"
-        >
-            {isHovered && (
-                <div 
-                    className={classes.case_glow}
-                    style={{
-                        left: `${mousePosition.x}px`,
-                        top: `${mousePosition.y}px`,
-                    }}
-                />
-            )}
-            <div className={classes.case_img}>
-                <img src={imgSrc} alt="" />
-                <div className={classes.case_img_tags}>
-                    {tags.map((tag, index) => (
-                        <Link key={index} to={'/'} className={classes.case_img_tag}>
-                            {tag}
-                        </Link>
-                    ))}
-                </div>
-            </div>
-            <div className={classes.case_bottom}>
-                <div className={classes.case_bottom_title}>{title}</div>
-                <div className={classes.case_bottom_desc}>
-                    {description}
-                </div>
-            </div>
-        </div>
+        <>
+            {type == "case" &&
+                <Link to={"/case"}
+                    className={`${classes.case} ${classes.type_case}`}
+                    ref={caseRef}
+                    onMouseMove={handleMouseMove}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                    data-cursor={type}
+                >
+                    {isHovered && (
+                        <div
+                            className={classes.case_glow}
+                            style={{
+                                left: `${mousePosition.x}px`,
+                                top: `${mousePosition.y}px`,
+                            }}
+                        />
+                    )}
+                    <div className={classes.case_img}>
+                        <img src={imgSrc} alt="" />
+                        <div className={classes.case_img_tags}>
+                            {tags.map((tag, index) => (
+                                <Link key={index} to={'/'} className={classes.case_img_tag} data-cursor="filter">
+                                    {tag}
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+                    <div className={classes.case_bottom}>
+                        <div className={classes.case_bottom_title}>{title}</div>
+                        <div className={classes.case_bottom_desc}>
+                            {description}
+                        </div>
+                    </div>
+                </Link>
+            }
+            {type == "banner" &&
+                <Link to={"/banner"}
+                    className={`${classes.case} ${classes.type_banner}`}
+                    ref={caseRef}
+                    onMouseMove={handleMouseMove}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                    data-cursor={type}
+                >
+                    {isHovered && (
+                        <div
+                            className={classes.case_glow}
+                            style={{
+                                left: `${mousePosition.x}px`,
+                                top: `${mousePosition.y}px`,
+                            }}
+                        />
+                    )}
+                    <div className={classes.banner_tags}>
+                        {tags.map((tag, index) => (
+                            <Link key={index} to={'/'} className={classes.banner_tag} data-cursor="filter">
+                                {tag}
+                            </Link>
+                        ))}
+                    </div>
+                    <div className={classes.banner_text}>
+                        <div className={classes.banner_title}>{title}</div>
+                        <div className={classes.banner_desc}>{description}</div>
+                    </div>
+
+                    <div className={classes.banner_img}>
+                        <div className={classes.banner_img_container}>
+                            <img src={imgSrc} alt="" />
+                        </div>
+                    </div>
+                </Link>
+            }
+            {type == "shop" &&
+                <Link to={"/shop"}
+                    className={`${classes.case} ${classes.type_case}`}
+                    ref={caseRef}
+                    onMouseMove={handleMouseMove}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                    data-cursor={type}
+                >
+                    {isHovered && (
+                        <div
+                            className={classes.case_glow}
+                            style={{
+                                left: `${mousePosition.x}px`,
+                                top: `${mousePosition.y}px`,
+                            }}
+                        />
+                    )}
+                    <div className={classes.case_img}>
+                        <img src={imgSrc} alt="" />
+                        <div className={classes.case_img_tags}>
+                            {tags.map((tag, index) => (
+                                <Link key={index} to={'/'} className={classes.case_img_tag} data-cursor="filter">
+                                    {tag}
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+                    <div className={classes.case_bottom}>
+                        <div className={classes.case_bottom_title}>{title}</div>
+                        <div className={classes.case_bottom_desc}>
+                            {description}
+                        </div>
+                    </div>
+                </Link>
+            }
+            {/* {type == "new" &&
+                <Link to={"/new"}
+                    className={`${classes.case} ${classes.type_case}`}
+                    ref={caseRef}
+                    onMouseMove={handleMouseMove}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                    data-cursor={type}
+                >
+                    {isHovered && (
+                        <div
+                            className={classes.case_glow}
+                            style={{
+                                left: `${mousePosition.x}px`,
+                                top: `${mousePosition.y}px`,
+                            }}
+                        />
+                    )}
+                    <div className={classes.case_img}>
+                        <img src={imgSrc} alt="" />
+                        <div className={classes.case_img_tags}>
+                            {tags.map((tag, index) => (
+                                <Link key={index} to={'/'} className={classes.case_img_tag} data-cursor="filter">
+                                    {tag}
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+                    <div className={classes.case_bottom}>
+                        <div className={classes.case_bottom_title}>{title}</div>
+                        <div className={classes.case_bottom_desc}>
+                            {description}
+                        </div>
+                    </div>
+                </Link>
+            } */}
+        </>
     );
 }
 
@@ -69,20 +182,79 @@ function Cases({ children, ...props }) {
             imgSrc: "/case-img-1.png",
             title: <>Магазин-каталог <br /> DR. PHONE</>,
             description: "Продумали внутреннюю логику для масштабного проекта, разработали систему и обновили дизайн",
-            tags: ["Веб-дизайн", "frontend", "2024"]
+            tags: ["Веб-дизайн", "frontend", "2024"],
+            type: 'case'
         },
         {
             imgSrc: "/case-img-2.png",
             title: <>Магазин-каталог <br /> ВЕЛО МОТО DRIVE</>,
             description: "Продумали внутреннюю логику для масштабного проекта, разработали систему и обновили дизайн",
-            tags: ["Веб-дизайн", "frontend", "2024"]
+            tags: ["Веб-дизайн", "frontend", "2024"],
+            type: 'case'
         },
         {
             imgSrc: "/case-img-3.png",
             title: "Сайт адвокатской коллегии",
             description: "Продумали внутреннюю логику для масштабного проекта, разработали систему и обновили дизайн",
-            tags: ["Веб-дизайн", "frontend", "2024"]
-        }
+            tags: ["Веб-дизайн", "frontend", "2024"],
+            type: 'case'
+        },
+        {
+            imgSrc: "/banner-img-1.png",
+            title: "Какая то акция",
+            description: <>Здесь описание. <br /> Сами карточки черные, подсвечиваются при наведении курсора</>,
+            tags: ["Акция"],
+            type: 'banner'
+        },
+        {
+            imgSrc: "/banner-img-2.png",
+            title: "Какая то акция",
+            description: <>Здесь описание. <br /> Сами карточки черные, подсвечиваются при наведении курсора</>,
+            tags: ["Акция"],
+            type: 'banner'
+        },
+        {
+            imgSrc: "/case-img-1.png",
+            title: <>Магазин-каталог <br /> DR. PHONE</>,
+            description: "Продумали внутреннюю логику для масштабного проекта, разработали систему и обновили дизайн",
+            tags: ["Веб-дизайн", "frontend", "2024"],
+            type: 'shop'
+        },
+        {
+            imgSrc: "/case-img-2.png",
+            title: <>Магазин-каталог <br /> ВЕЛО МОТО DRIVE</>,
+            description: "Продумали внутреннюю логику для масштабного проекта, разработали систему и обновили дизайн",
+            tags: ["Веб-дизайн", "frontend", "2024"],
+            type: 'shop'
+        },
+        {
+            imgSrc: "/case-img-3.png",
+            title: "Сайт адвокатской коллегии",
+            description: "Продумали внутреннюю логику для масштабного проекта, разработали систему и обновили дизайн",
+            tags: ["Веб-дизайн", "frontend", "2024"],
+            type: 'shop'
+        },
+        {
+            imgSrc: "/case-img-1.png",
+            title: <>Магазин-каталог <br /> DR. PHONE</>,
+            description: "Продумали внутреннюю логику для масштабного проекта, разработали систему и обновили дизайн",
+            tags: ["Веб-дизайн", "frontend", "2024"],
+            type: 'new'
+        },
+        {
+            imgSrc: "/case-img-2.png",
+            title: <>Магазин-каталог <br /> ВЕЛО МОТО DRIVE</>,
+            description: "Продумали внутреннюю логику для масштабного проекта, разработали систему и обновили дизайн",
+            tags: ["Веб-дизайн", "frontend", "2024"],
+            type: 'new'
+        },
+        {
+            imgSrc: "/case-img-3.png",
+            title: "Сайт адвокатской коллегии",
+            description: "Продумали внутреннюю логику для масштабного проекта, разработали систему и обновили дизайн",
+            tags: ["Веб-дизайн", "frontend", "2024"],
+            type: 'new'
+        },
     ];
 
     return (
@@ -94,8 +266,6 @@ function Cases({ children, ...props }) {
                     ))}
 
                     {/* 
-                    <div className={`${classes.case} ${classes.type_banner}`}>banner</div>
-
                     <div className={`${classes.case} ${classes.type_new}`}>new</div>
 
                     <div className={`${classes.case} ${classes.type_shop}`}>shop</div> */}
--- src/Components/Cursor/CustomCursor.jsx
+++ src/Components/Cursor/CustomCursor.jsx
@@ -23,60 +23,61 @@ const CONFIG = {
     link: 50,
     media: 80,
     case: 70,
+    filter: 60,
   },
-  
+
   // Плавность движения (0 = плавнее, 1 = мгновенно)
   speed: 0.25,
-  
+
   // Множитель скорости для расчёта растяжения
   velocityMultiplier: 4,
-  
+
   // Максимальная скорость для нормализации
   maxVelocity: 150,
-  
+
   // Сила сжатия (0.5 = максимум)
   squeezeStrength: 0.5,
-  
+
   // Порог скорости для поворота (чтобы избежать дрожания)
   rotationThreshold: 10,
 };
 
 const CustomCursor = () => {
   const cursorRef = useRef(null);
-  
+
   useEffect(() => {
     // Проверка на touch-устройство
     const hasCoarsePointer = window.matchMedia('(pointer: coarse)').matches;
     if (hasCoarsePointer) return;
-    
+
     // Проверка prefers-reduced-motion
     const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
-    
+
     if (!cursorRef.current) return;
-    
+
     const circle = cursorRef.current;
     const cursorInner = circle.querySelector('.cursor-inner');
     if (!cursorInner) return;
-    
+
     // Отслеживание позиций мыши
     const mouse = { x: 0, y: 0 };
     const previousMouse = { x: 0, y: 0 };
     const circlePos = { x: 0, y: 0 };
-    
+
     // Отслеживание scale и rotation
     let currentScale = 0;
     let currentAngle = 0;
-    
+
     // Флаг первой инициализации
     let isFirstMove = true;
-    
+
     let rafId;
-    
+
     // Обновление позиции мыши
     const updateMousePosition = (e) => {
       mouse.x = e.clientX;
       mouse.y = e.clientY;
-      
+
       // При первом движении мыши - телепортируем курсор в позицию мыши
       if (isFirstMove) {
         circlePos.x = e.clientX;
@@ -86,103 +87,103 @@ const CustomCursor = () => {
         isFirstMove = false;
       }
     };
-    
+
     // Главный цикл анимации
     const tick = () => {
       // === ДВИЖЕНИЕ ===
       // Плавное следование за мышью
       circlePos.x += (mouse.x - circlePos.x) * CONFIG.speed;
       circlePos.y += (mouse.y - circlePos.y) * CONFIG.speed;
-      
+
       const translateTransform = `translate(${circlePos.x}px, ${circlePos.y}px)`;
-      
+
       // === СЖАТИЕ (SQUEEZE) И УМЕНЬШЕНИЕ ===
       let scaleTransform = 'scale(1, 1)';
       let rotateTransform = 'rotate(0deg)';
       let overallScale = 1; // Общий масштаб шарика
-      
+
       if (!prefersReducedMotion) {
         // 1. Вычисляем изменение позиции мыши
         const deltaMouseX = mouse.x - previousMouse.x;
         const deltaMouseY = mouse.y - previousMouse.y;
-        
+
         // Обновляем предыдущую позицию
         previousMouse.x = mouse.x;
         previousMouse.y = mouse.y;
-        
+
         // 2. Вычисляем скорость мыши (формула Пифагора)
         const mouseVelocity = Math.min(
           Math.sqrt(deltaMouseX ** 2 + deltaMouseY ** 2) * CONFIG.velocityMultiplier,
           CONFIG.maxVelocity
         );
-        
+
         // 3. Конвертируем скорость в значение scale [0, 0.5] для растяжения
         const scaleValue = (mouseVelocity / CONFIG.maxVelocity) * CONFIG.squeezeStrength;
-        
+
         // 4. Плавно обновляем текущий scale
         currentScale += (scaleValue - currentScale) * CONFIG.speed;
-        
+
         // 5. Уменьшаем общий размер шарика при увеличении скорости
         // При максимальной скорости шарик уменьшается до 70% от исходного размера
         const shrinkAmount = (mouseVelocity / CONFIG.maxVelocity) * 0.3; // 0.3 = 30% уменьшения
         overallScale = 1 - shrinkAmount;
-        
+
         // 6. Создаём трансформацию scale (растяжение + общее уменьшение)
         scaleTransform = `scale(${(1 + currentScale) * overallScale}, ${(1 - currentScale) * overallScale})`;
-        
+
         // === ПОВОРОТ (ROTATE) ===
         // 1. Вычисляем угол с помощью atan2
         const angle = (Math.atan2(deltaMouseY, deltaMouseX) * 180) / Math.PI;
-        
+
         // 2. Применяем порог для уменьшения дрожания при низкой скорости
         if (mouseVelocity > CONFIG.rotationThreshold) {
           currentAngle = angle;
         }
-        
+
         // 3. Создаём трансформацию rotate
         rotateTransform = `rotate(${currentAngle}deg)`;
       }
-      
+
       // Применяем трансформации
       // К родителю - только позиция
       circle.style.transform = translateTransform;
-      
+
       // К внутреннему элементу - центрирование, поворот и растяжение
       // translate(-50%, -50%) центрирует элемент относительно позиции курсора
       cursorInner.style.transform = `translate(-50%, -50%) ${rotateTransform} ${scaleTransform}`;
       cursorInner.style.transformOrigin = 'center center';
-      
+
       // Запрашиваем следующий кадр
       rafId = requestAnimationFrame(tick);
     };
-    
+
     window.addEventListener('mousemove', updateMousePosition);
     rafId = requestAnimationFrame(tick);
-    
+
     return () => {
       window.removeEventListener('mousemove', updateMousePosition);
       if (rafId) cancelAnimationFrame(rafId);
     };
   }, []);
-  
+
   // Обработка data-cursor атрибутов
   useEffect(() => {
     if (!cursorRef.current) return;
-    
+
     const handlePointerOver = (e) => {
       const target = e.target;
       const cursorElement = target.closest('[data-cursor]');
-      
+
       if (cursorElement && cursorRef.current) {
         const cursorMode = cursorElement.getAttribute('data-cursor');
         const cursorLabel = cursorElement.getAttribute('data-cursor-label');
-        
+
         cursorRef.current.setAttribute('data-mode', cursorMode);
-        
+
         if (cursorLabel && (cursorMode === 'media' || cursorMode === 'case')) {
           cursorRef.current.setAttribute('data-label', cursorLabel);
         }
-        
+
         // Для slider определяем направление
         if (cursorMode === 'slider') {
           const rect = cursorElement.getBoundingClientRect();
@@ -193,9 +194,9 @@ const CustomCursor = () => {
               cursorRef.current.setAttribute('data-slider-direction', direction);
             }
           };
-          
+
           updateDirection(e.clientX);
-          
+
           const handleMove = (moveE) => updateDirection(moveE.clientX);
           cursorElement.addEventListener('mousemove', handleMove);
           cursorElement.addEventListener('mouseleave', () => {
@@ -204,30 +205,30 @@ const CustomCursor = () => {
         }
       }
     };
-    
+
     const handlePointerOut = (e) => {
       const target = e.target;
       const cursorElement = target.closest('[data-cursor]');
-      
+
       if (cursorElement && cursorRef.current) {
         cursorRef.current.setAttribute('data-mode', 'default');
         cursorRef.current.removeAttribute('data-label');
       }
     };
-    
+
     document.addEventListener('pointerover', handlePointerOver);
     document.addEventListener('pointerout', handlePointerOut);
-    
+
     return () => {
       document.removeEventListener('pointerover', handlePointerOver);
       document.removeEventListener('pointerout', handlePointerOut);
     };
   }, []);
-  
+
   // Проверка на touch-устройство для рендера
   const hasCoarsePointer = typeof window !== 'undefined' && window.matchMedia('(pointer: coarse)').matches;
   if (hasCoarsePointer) return null;
-  
+
   return (
     <div
       ref={cursorRef}
@@ -242,12 +243,12 @@ const CustomCursor = () => {
           </svg>
           <span className="cursor-label"></span>
         </div>
-        
+
         {/* Контент для режима drag */}
         <div className="cursor-drag-content">
           <span className="cursor-label">Drag</span>
         </div>
-        
+
         {/* Контент для режима slider */}
         <div className="cursor-slider-content">
           <svg className="cursor-arrow-icon" viewBox="0 0 24 24" fill="none">
@@ -256,10 +257,28 @@ const CustomCursor = () => {
           </svg>
         </div>
       </div>
-      
+
       {/* Контент для режима case - вынесен наружу, чтобы не вращался */}
       <div className="cursor-case-content">
-        <span className="cursor-label">Открыть</span>
+        <span className="cursor-label">Открыть <br /> кейс</span>
+      </div>
+
+      {/* Контент для режима filter - вынесен наружу, чтобы не вращался */}
+      <div className="cursor-filter-content">
+        {/* <img src="/filter.png" alt="" /> */}
+        <span className="cursor-label">Настроить <br /> фильтр</span>
+      </div>
+
+      <div className="cursor-banner-content">
+        <span className="cursor-label">Открыть <br /> баннер</span>
+      </div>
+
+      <div className="cursor-shop-content">
+        <span className="cursor-label">Открыть <br /> в магазине</span>
+      </div>
+
+      <div className="cursor-new-content">
+        <span className="cursor-label">Открыть <br /> новость</span>
       </div>
     </div>
   );
